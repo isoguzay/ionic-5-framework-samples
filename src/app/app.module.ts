1a94e1720c82e056f0e4d0473a092a8410a38a59
ismailoguzhanay <isoguzay@gmail.com>
2020-04-09 12:53:39 +0300

    2. ionic folder architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserFilterPipe } from './utils/pipes/user-filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserFilterPipe],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
